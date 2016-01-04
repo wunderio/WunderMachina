8fe6de8b87dc86c8de08703c8e2512528cea5add
Janne Koponen <janne.koponen@wunderkraut.com>
2016-01-04 20:16:52 +0200

    Improved vagrantfile.

--- vagrantfile.rb
+++ vagrantfile.rb
@@ -10,11 +10,7 @@ INSTANCE_MEM      = settings['mem']
 INSTANCE_CPUS     = settings['cpus']
 INSTANCE_IP       = settings['ip']
 INSTANCE_BOX      = settings['box']
-ANSIBLE_INVENTORY = "ansible/inventory"
 
-# Write the inventory file for ansible
-FileUtils.mkdir_p dir + ANSIBLE_INVENTORY
-File.open(dir + ANSIBLE_INVENTORY + "/hosts", 'w') { |file| file.write("[vagrant]\n" + INSTANCE_IP) }
 # Link the ansible playbook
 unless File.exist?(dir + "ansible/playbook/vagrant.yml")
 	FileUtils.ln_s "../../conf/vagrant.yml", dir + "ansible/playbook/vagrant.yml"
@@ -41,10 +37,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	########################################
 
 	config.vm.hostname = INSTANCE_HOSTNAME
-	config.vm.box      = "centos-6.6-x86_64-v0"
+	config.vm.box      = INSTANCE_BOX
 
 	config.vm.network :private_network, ip: INSTANCE_IP
-#	config.ssh.private_key_path = "~/.vagrant.d/insecure_private_key"
 
 	# Sync folders
 	config.vm.synced_folder ".", "/vagrant", type: :nfs
@@ -63,9 +58,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	# Configuration for virtualbox
 	########################################
 
-	config.vm.provider "virtualbox" do |v, override|
-		override.vm.box_url = "https://www.dropbox.com/s/mo7tjw15z5ep6p6/vb-centos-6.6-x86_64-v0.box?dl=1"
-	end
+	#config.vm.provider "virtualbox" do |v, override|
+	#	override.vm.box_url = "https://www.dropbox.com/s/mo7tjw15z5ep6p6/vb-centos-6.6-x86_64-v0.box?dl=1"
+	#end
 
 	config.vm.provider :virtualbox do |vb|
 		vb.name = INSTANCE_NAME
@@ -98,9 +93,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	# Configuration for vmware_fusion
 	########################################
 
-	config.vm.provider "vmware_fusion" do |v, override|
-		override.vm.box_url = "https://www.dropbox.com/s/tcp23ka9hlhhsel/vm-centos-6.6-x86_64-v0.box?dl=1"
-	end
+	#config.vm.provider "vmware_fusion" do |v, override|
+	#	override.vm.box_url = "https://www.dropbox.com/s/tcp23ka9hlhhsel/vm-centos-6.6-x86_64-v0.box?dl=1"
+	#end
 
 	config.vm.provider "vmware_fusion" do |vb|
 		vb.name = INSTANCE_NAME
@@ -114,12 +109,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
 	config.vm.provision "ansible" do |ansible|
 		#ansible.verbose        = "v"
-		ansible.inventory_path = ANSIBLE_INVENTORY
-		ansible.extra_vars     = dir + "conf/variables.yml"
+    ansible.extra_vars     = dir + "conf/variables.yml"
 		ansible.playbook       = dir + "ansible/playbook/vagrant.yml"
 		ansible.limit          = "all"
 	end
-
-	config.vm.provision :shell, :path => "ansible/shell/provision.sh"
 
 end
