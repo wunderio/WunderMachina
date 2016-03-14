d28a119b3c08124644bad655d9dc12472d4b44c6
federico prato <federico.prato@wunderkraut.com>
2016-03-14 08:52:03 +0200

    Should put you into current if present, into drupal if not

--- shell/provision.sh
+++ shell/provision.sh
@@ -2,3 +2,4 @@
 
 echo export WKV_SITE_ENV="vagrant" > /etc/profile.d/wkv.sh
 echo "export XDEBUG_CONFIG=\"remote_host=\${SSH_CLIENT%% *}\"" > /etc/profile.d/xdebug.sh
+echo "if [ -d \"/vagrant/drupal/current\" ]; then cd /vagrant/drupal/current; else cd /vagrant/drupal; fi" >> /home/vagrant/.bashrc
