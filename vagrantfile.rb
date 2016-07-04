58d1d189dc075b6204b1ab32fee337b3b836656e
Aleksi Johansson <aleksi.johansson@wunderkraut.com>
2016-07-04 15:06:07 +0300

    Added vagrant-hostmanager configuration.

--- vagrantfile.rb
+++ vagrantfile.rb
@@ -25,6 +25,12 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	# Default configuration
 	########################################
 
+	# vagrant-hostmanager
+	config.hostmanager.enabled = true
+  config.hostmanager.manage_host = true
+  config.hostmanager.ignore_private_ip = false
+  config.hostmanager.include_offline = true
+
 	config.vm.hostname = INSTANCE_HOSTNAME
 	config.vm.box      = INSTANCE_BOX
 
