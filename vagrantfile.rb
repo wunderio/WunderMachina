dafccf76db5311abe8f36f46fbc93d0b38d61da3
Aleksi Johansson <aleksi.johansson@wunderkraut.com>
2016-07-04 15:32:32 +0300

    Wrapped the configuration to Vagrant.has_plugin so that it's only taken into consideration if the plugin is installed.

--- vagrantfile.rb
+++ vagrantfile.rb
@@ -26,10 +26,12 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	########################################
 
 	# vagrant-hostmanager
-	config.hostmanager.enabled = true
-  config.hostmanager.manage_host = true
-  config.hostmanager.ignore_private_ip = false
-  config.hostmanager.include_offline = true
+	if Vagrant.has_plugin?("vagrant-hostmanager")
+		config.hostmanager.enabled = true
+		config.hostmanager.manage_host = true
+		config.hostmanager.ignore_private_ip = false
+		config.hostmanager.include_offline = true
+  end
 
 	config.vm.hostname = INSTANCE_HOSTNAME
 	config.vm.box      = INSTANCE_BOX
